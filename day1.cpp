c7db8e7771feca3b8a95baa68f63d83dca033c8c
Carloslecval <carloslecval03@gmail.com>
2024-12-01 19:49:43 -0600

    day 1 part two

--- day1.cpp
+++ day1.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 
-int main()
-{
+void partOne() {
     freopen("input.txt", "r", stdin);
     int x, y;
     int sum = 0;
@@ -22,4 +21,26 @@ int main()
         sum += abs(left[i] - right[i]);
     }
     cout << sum << endl;
+}
+
+void partTwo() {
+    freopen("input.txt", "r", stdin);
+    int x, y;
+    int sum = 0;
+    vector<int> left;
+    unordered_map<int, int> right;
+    
+    while (cin >> x >> y) {
+        left.push_back(x);
+        right[y]++;
+    }
+    for(int i = 0; i < left.size(); i++) {
+        sum += left[i] * right[left[i]];
+    }
+    cout << sum << endl;
+}
+
+int main()
+{
+    partTwo();
 }
